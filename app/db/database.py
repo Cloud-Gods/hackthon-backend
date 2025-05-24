86e622be4efe3c192bcec074e4ee82661057d42a
Yeison Cano <yeisonc2002@gmail.com>
2025-05-23 22:41:19 -0500

    add: Se agrego al conexion a la base de datos, ademas se creo el logger y el patron singleton

--- app/db/database.py
+++ app/db/database.py
@@ -1,13 +1,39 @@
-# app/db/database.py
-
-from pymongo import MongoClient
+import mysql.connector
+from mysql.connector import Error
+import sys
 import os
-from dotenv import load_dotenv
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+from app.models.logger import Logger
 
-load_dotenv()
+#Clase para mnejar la base de datos postgres
+class ConexionDB:
 
-client = MongoClient(os.getenv("DATABASE_URL"))
-db = client[os.getenv("DATABASE_NAME")]
+    #Constructor
+    def __init__(self):
+        self.log = Logger("logs/Conexion.log").get_logger()
+        self.host = "localhost"
+        self.port = 3306
+        self.user = "root"
+        self.password = "Yeisoncano123"
+        self.database = "BotTech"
+    
+    #Metodo para conectar a la base de datos
+    def conectar(self):
+        try:
+            con = mysql.connector.connect(
+                host=self.host,
+                port=self.port,
+                user=self.user,
+                password=self.password,
+                database=self.database
+            )
+            if con.is_connected():
+                self.log.info("Conexión exitosa a la base de datos")
+                return con
+            else:
+                self.log.error("Error al conectar a la base de datos")
+                return None
+        except Exception as ex:
+            self.log.error(f"Error al conectar a la base de datos: {ex}")
 
-def get_user_collection():
-    return db["users"]
+ConexionDB().conectar()
